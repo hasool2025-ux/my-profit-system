d9d12869616b3bd7b6dc1ba9568bf29420892a97
hasool2025-ux <hasool2025@gmail.com>
2026-07-08 08:11:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("System running 24/7")
+from flask import Flask
+import threading
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "System is Online and Running 24/7"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    t = threading.Thread(target=run)
+    t.start()
+    while True:
+        print("الخوارزمية تعمل الآن وتراقب البيانات...")
+        time.sleep(60)
